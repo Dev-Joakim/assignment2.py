3faaf10299486eb1cf419b448a867b04b971727d
Dev-Joakim <joakimmutinda100@gmail.com>
2025-03-16 18:11:54 +0300

    this is python assignment

---
+++ prac -2.py
@@ -0,0 +1,5 @@
+temperature= 10
+if temp > 25:
+  print("it's a hot day")
+else:
+  print("its a cool day!")
---
+++ prac-1.py
@@ -0,0 +1,14 @@
+age =25
+height = 5.75
+name ="charlie"
+is_student = True
+
+print("Age:",age)
+print("Height:",height)
+print("Name:",name)
+print("Is Student:",is_student)
+
+year =2025
+print(type(year),year)
+year ='twenty twenty five'
+print(type(year),year)
